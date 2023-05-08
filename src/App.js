b3ef85ffb8e4809a175beded4d72beb08ad0c57c
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-05-08 13:02:51 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ function App() {
     <div className="App">
       <h1>The Wild Side</h1>
       <h2>Hair Salon</h2>
-      <img className='logo' src={logo} alt='The Wild Side Salon'/>
+      <img className='logo' src={logo} alt='The Wild Side Salon' />
       <div className='bookCont'>
-      <a href='https://thewildside.mylocalsalon.com/OnlineBooking/' target='_blank' rel="noreferrer">
-      <button class="btn-101">
-  Book Appointment!
-  <svg>
-    <defs>
-      <filter id="glow">
-        <fegaussianblur result="coloredBlur" stddeviation="5"></fegaussianblur>
-        <femerge>
-          <femergenode in="coloredBlur"></femergenode>
-          <femergenode in="coloredBlur"></femergenode>
-          <femergenode in="coloredBlur"></femergenode>
-          <femergenode in="SourceGraphic"></femergenode>
-        </femerge>
-      </filter>
-    </defs>
-    <rect />
-  </svg>
-</button>
-      </a>
+        <a href='https://thewildside.mylocalsalon.com/OnlineBooking/' target='_blank' rel="noreferrer">
+          <button class="btn-101">
+            BOOK APPOINTMENT!
+            <svg>
+              <defs>
+                <filter id="glow">
+                  <fegaussianblur result="coloredBlur" stddeviation="5"></fegaussianblur>
+                  <femerge>
+                    <femergenode in="coloredBlur"></femergenode>
+                    <femergenode in="coloredBlur"></femergenode>
+                    <femergenode in="coloredBlur"></femergenode>
+                    <femergenode in="SourceGraphic"></femergenode>
+                  </femerge>
+                </filter>
+              </defs>
+              <rect />
+            </svg>
+          </button>
+        </a>
 
       </div>
     </div>
